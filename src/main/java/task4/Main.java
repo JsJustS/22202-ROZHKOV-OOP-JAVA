ab973cbada0efacb6bd4f6fd55b749cb0b0d3dce
Just <grivten@yandex.ru>
2024-02-24 11:21:10 +0700

    screen

--- src/main/java/task4/Main.java
+++ src/main/java/task4/Main.java
@@ -2,11 +2,16 @@ package task4;
 
 import task4.util.ArgParser;
 import task4.util.Config;
+import task4.util.Screen;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         ArgParser parser = new ArgParser(args);
         Config cfg = (parser.length() == 0) ? Config.GENERAL : Config.load(parser.get(0).getAsString());
 
+        Factory factory = new Factory();
+        Screen screen = new Screen(factory);
     }
 }
---
+++ src/main/java/task4/util/IScreen.java
@@ -0,0 +1,14 @@
+package task4.util;
+
+public interface IScreen {
+
+    void updateBodyCount(int count);
+
+    void updateAutoCount(int count);
+
+    void updateMotorCount(int count);
+
+    void updateAccessoryCount(int count);
+
+    void updateGeneralCount(int count);
+}
