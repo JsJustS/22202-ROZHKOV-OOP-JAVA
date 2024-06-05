a0b60037167ebcbfa3c31a958f0d6102c77a8812
Just <grivten@yandex.ru>
2024-06-05 10:25:54 +0700

    optimization

--- src/main/java/task3/util/ResourceManager.java
+++ src/main/java/task3/util/ResourceManager.java
@@ -5,8 +5,11 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResourceManager {
+    private static final Map<String, BufferedImage> loadedResources = new HashMap<>();
     public static BufferedImage getMissingTexture() {
         BufferedImage texture = new BufferedImage(2, 2, BufferedImage.TYPE_INT_BGR);
         texture.setRGB(0, 0, Color.MAGENTA.getRGB());
@@ -17,10 +20,14 @@ public class ResourceManager {
     }
 
     public static BufferedImage loadImage(String filename) {
+        if (loadedResources.containsKey(filename)) {
+            return loadedResources.get(filename);
+        }
         BufferedImage loadedImage;
         try (InputStream stream = ResourceManager.class.getResourceAsStream("/" + filename)) {
             if (stream == null) throw new IOException();
             loadedImage = ImageIO.read(stream);
+            loadedResources.put(filename, loadedImage);
         } catch (IOException ignored) {
             loadedImage = ResourceManager.getMissingTexture();
         }
